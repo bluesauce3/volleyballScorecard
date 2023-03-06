23e706bd6560be660c14e201655663096b9e2f0c
Theo Baker <theocgbaker@gmail.com>
2023-03-06 19:41:37 +1300

    added team sheet submission forms using table

---
+++ app.js
@@ -0,0 +1,6 @@
+function submitTeamSheet(){ //get both team sheets when submitted to use for scoring
+    let teamSheet = {teamA:[], teamB:[]}
+    teamA = document.getElementsByClassName("playernames-teamA");
+    teamB = document.getElementsByClassName("playernames-teamB");
+    console.log(teamA[2].value);
+}
