2530d6cf7d6910ad00599fb0f98b526167222ffb
bluesauce3 <theocgbaker@gmail.com>
2023-03-07 18:41:04 +1300

    made able to use inputted content using DOM

--- app.js
+++ app.js
@@ -1,6 +1,34 @@
 function submitTeamSheet(){ //get both team sheets when submitted to use for scoring
-    let teamSheet = {teamA:[], teamB:[]}
-    teamA = document.getElementsByClassName("playernames-teamA");
-    teamB = document.getElementsByClassName("playernames-teamB");
-    console.log(teamA[2].value);
+    let teamSheet = {'teamAnames':NaN, 'teamBnames':NaN, 'teamAnumbers':NaN, 'teamBnumbers':NaN}
+    teamAnamedata = document.getElementsByClassName("playername teamA");
+    teamBnamedata = document.getElementsByClassName("playername teamB");
+    teamAnumberdata = document.getElementsByClassName("playernumber teamA");
+    teamBnumberdata = document.getElementsByClassName("playernumber teamB");
+    
+    //put both team names into team sheet
+    teamAPlayerNames = [];
+    for (let i = 0; i < teamAnamedata.length; i++) {
+        teamAPlayerNames.push(teamAnamedata[i].textContent);
+    }
+    teamSheet['teamAnames'] = teamAPlayerNames.slice();
+
+    teamBPlayerNames = [];
+    for (let i = 0; i < teamBnamedata.length; i++) {
+        teamBPlayerNames.push(teamBnamedata[i].textContent);
+    }
+    teamSheet['teamBnames'] = teamBPlayerNames.slice();
+
+    teamAPlayerNumbers = [];
+    for (let i = 0; i < teamAnumberdata.length; i++) {
+        teamAPlayerNumbers.push(teamAnumberdata[i].textContent);
+    }
+    teamSheet['teamAnumbers'] = teamAPlayerNumbers.slice();
+
+    teamBPlayerNumbers = [];
+    for (let i = 0; i < teamBnumberdata.length; i++) {
+        teamBPlayerNumbers.push(teamBnumberdata[i].textContent);
+    }
+    teamSheet['teamBnumbers'] = teamBPlayerNumbers.slice();
+
+    console.log(teamSheet);
 }
